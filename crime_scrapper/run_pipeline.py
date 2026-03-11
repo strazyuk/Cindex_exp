973c4e6930a211759e516856893e9b18a9d69ec7
strazyuk <129723342+strazyuk@users.noreply.github.com>
2026-03-11 14:58:34 +0600

    all deleted

--- crime_scrapper/run_pipeline.py
+++
@@ -1,9 +0,0 @@
-from allScrappers.dailystar import get_dailystar_articles
-from NlpClassifier.predictor import CrimePredictor
-
-predictor = CrimePredictor()
-articles = get_dailystar_articles()
-
-for article in articles:
-    category = predictor.predict(article["title"])
-    print(f"[{category.upper()}] {article['title']}")
